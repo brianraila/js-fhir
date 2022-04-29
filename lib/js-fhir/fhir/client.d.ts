d2b5b9bce905ac97b8540c4a4e9397f49818fc6f
Amolo <railamolo@gmail.com>
2022-04-29 10:04:24 +0300

    Types is not easy

---
+++ lib/js-fhir/fhir/client.d.ts
@@ -0,0 +1,46 @@
+declare module 'index';
+
+export type BasicAuth = {
+    user: string
+    pass: string
+}
+
+export type Bearer = {
+    token: string
+}
+
+export type fetchParams = {
+    baseUrl:string
+    auth: Bearer | BasicAuth | null
+    authType: string
+    method: string
+    url: string
+    headers: any
+    data: string
+}
+
+export type Resource = {
+    resourceType: string
+    id: string
+    active: Boolean
+}
+
+export interface Res {
+    status: Number
+    statusText: string
+}
+
+export interface TextResponse extends Res {
+    content: string
+}
+
+export interface JSONResponse extends Res {
+    content: any
+}
+
+export interface ClientError extends Res {
+    status: Number
+    statusText: string
+    content: any
+}
+
